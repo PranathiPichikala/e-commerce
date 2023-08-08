7cefbd60babb65bca0f8509757c5840335488b1b
Siddarth <srinivas@presentiencetech.com>
2023-08-08 16:10:57 +0530

    Empty cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,11 @@
 import "./css/Cart.css"
 import { BiSearch } from "react-icons/bi"
 import { useState } from "react"
+import EmptyCart from "../assets/Empty Cart.webp"
 
 const Cart = () => {
     const [search, setSearch] = useState("")
+    const [activemenu, setActivemenu] = useState("e-cart")
 
     return (
         <div className="_5pko">
@@ -17,15 +19,22 @@ const Cart = () => {
             </div>
             <div className="cart">
                 <div className="navigate">
-                    <div>
+                    <div onClick={() => setActivemenu("e-cart")} className={activemenu === "e-cart" ? "active" : undefined}>
                         e-cart
                     </div>
-                    <div>
+                    <div onClick={() => setActivemenu("grocery")} className={activemenu === "grocery" ? "active" : undefined}>
                         Grocery
                     </div>
                 </div>
                 <div className="container">
-
+                    <div className="matinee-were">
+                        <img src={EmptyCart} alt="" />
+                    </div>
+                    <div className="eclair-eel">
+                        <h4>Missing Cart items?</h4>
+                        <span>Login to see the items you added previously</span>
+                        <button>Login</button>
+                    </div>
                 </div>
             </div>
         </div>
